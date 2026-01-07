26f6c465c1bb43d1713e731331fcb97ab01b818d
Kevin A Dobbin <94518887+KEVINADOBBIN@users.noreply.github.com>
2026-01-07 13:57:33 +0530

    Add files via upload

---
+++ experiment11_sysdate_display.sql
@@ -0,0 +1,7 @@
+-- SQLite version: Display Current Date and Time
+
+-- Display current date
+SELECT DATE('now') AS Current_Date;
+
+-- Display current date and time
+SELECT CURRENT_TIMESTAMP AS Current_DateTime;
